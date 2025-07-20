1490278f5b18610e309c318ea6ee72e4231725a8
Daniel Park <intheimageofyeshua@gmail.com>
2025-07-20 10:21:08 +0900

    Factorial Recursive

--- control-flow/src/main.rs
+++ control-flow/src/main.rs
@@ -81,6 +81,27 @@ fn main() {
 
     countdown(5);
 
+    println!("Factorial: {}", factorial_iterative(5));
+    println!("Factorial: {}", factorial_recursive(5));
+}
+
+fn factorial_recursive(number: i32) -> i32 {
+    if number == 1 {
+        return 1;
+    }
+    number * factorial_recursive(number - 1)
+}
+
+fn factorial_iterative(number: i32) -> i32 {
+    let mut product = 1;
+    let mut count = number;
+
+    while count > 0 {
+        product *= count;
+        count -= 1;
+    }
+
+    product
 }
 
 fn countdown(seconds: i32) {
