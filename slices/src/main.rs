6ede56cd97098062ba6cb0aa296ed4cf17f3162c
Daniel Park <intheimageofyeshua@gmail.com>
2025-07-27 16:34:12 +0900

    Emoji Slice

--- slices/src/main.rs
+++ slices/src/main.rs
@@ -1,10 +1,24 @@
 fn main() {
     // slices
-    let bible_hero = String::from("Joshua Park");
+    // let bible_hero = String::from("Joshua Park"); // heap allocated string
+    let bible_hero = "Joshua Park"; // memory in executable file
     let string_reference = &bible_hero;
     println!("{string_reference}");
 
     let first_name = &bible_hero[0..6]; // byte range
     let last_name = &bible_hero[7..11]; // each byte represents a character
     println!("{first_name} {last_name}");
+
+    let whose_name = {
+        let betrayer = "Judas Iscariot";
+        &betrayer[0..5] // creates separate reference and return it >> no dangling reference 
+    };
+    println!("{whose_name}");
+
+    let food = "apricot";
+    println!("length: {}", food.len());
+    let fruit_slice = &food[0..3];
+    println!("length: {}", fruit_slice.len());
+    let smile = "😊";
+    println!("emoji length: {}", smile.len()); // emoji occupies 4-bytes which cannot be sliced
 }
