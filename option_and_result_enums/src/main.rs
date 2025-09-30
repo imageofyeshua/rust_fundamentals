58896a67379e60b33f452b5f4b3a4912eb56db10
Daniel Park <intheimageofyeshua@gmail.com>
2025-09-30 18:21:05 +0900

    Returning an Option Enum

--- option_and_result_enums/src/main.rs
+++ option_and_result_enums/src/main.rs
@@ -46,6 +46,25 @@ fn main() {
 
     play(_piano);
     play(_invalid_instrument);
+
+    // returning an option enum from a function
+    let availability = is_item_in_stock(true, false);
+    println!("{availability:?}");
+
+    match availability {
+        Option::Some(value) => println!("Item is available: {value}"),
+        Option::None => println!("Your item doesn't exist in our system"),
+    }
+}
+
+fn is_item_in_stock(item_is_in_system: bool, item_is_in_stock: bool) -> Option<bool> {
+    if item_is_in_system && item_is_in_stock {
+        Option::Some(true)
+    } else if item_is_in_system {
+        Option:: Some(false)
+    } else {
+        Option::None
+    }
 }
 
 fn play(instrument_option: Option<&String>) {
