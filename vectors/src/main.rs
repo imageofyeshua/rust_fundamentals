5f1d43c50c8b85474f7bed2060dc13897c8d758a
Daniel Park <intheimageofyeshua@gmail.com>
2025-10-02 08:03:12 +0900

    Vector Push Pop Remove Insert

--- vectors/src/main.rs
+++ vectors/src/main.rs
@@ -5,4 +5,16 @@ fn main() {
     println!("{cake_diameters:?}");
     println!("{noodle_diameters:?}");
     println!("{pasta:?}");
+
+    let mut pancake_diameters = vec![8, 10, 12, 15];
+    pancake_diameters.push(16);
+    pancake_diameters.push(19);
+    pancake_diameters.insert(0, 4);
+
+    let last_pancake_diameter = pancake_diameters.pop();
+    let third_pancake_diameter = pancake_diameters.remove(2);
+
+    println!("{pancake_diameters:?}");
+    println!("pancake diameter: {:?}", last_pancake_diameter.unwrap());
+    println!("third pancake diameter: {:?}", third_pancake_diameter);
 }
