44298cf8072dc772f09f5c2b5f731ebd412594ac
Daniel Park <intheimageofyeshua@gmail.com>
2025-10-01 21:02:39 +0900

    Create Vector

---
+++ vectors/src/main.rs
@@ -0,0 +1,8 @@
+fn main() {
+    let cake_diameters: Vec<i8> = Vec::new();
+    let noodle_diameters = Vec::<i16>::new();
+    let pasta = vec!["Rigatoni", "Angel hair", "Fettucine"];
+    println!("{cake_diameters:?}");
+    println!("{noodle_diameters:?}");
+    println!("{pasta:?}");
+}
