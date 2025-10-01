725872d7e78eae6474ba9ca324b56a76d186524d
Daniel Park <intheimageofyeshua@gmail.com>
2025-10-02 08:14:45 +0900

    Reading Vector Elements

--- vectors/src/main.rs
+++ vectors/src/main.rs
@@ -14,7 +14,24 @@ fn main() {
     let last_pancake_diameter = pancake_diameters.pop();
     let third_pancake_diameter = pancake_diameters.remove(2);
 
-    println!("{pancake_diameters:?}");
     println!("pancake diameter: {:?}", last_pancake_diameter.unwrap());
     println!("third pancake diameter: {:?}", third_pancake_diameter);
+    println!("{pancake_diameters:?}");
+
+    let pepperoni = String::from("Pepperoni");
+    let mushroom = String::from("Mushroom");
+    let radish = String::from("Radish");
+    let cake_toppings = vec![pepperoni, mushroom, radish];
+
+    let value = pancake_diameters[2];
+    println!("pancake diameter: {value}");
+
+    let reference = &cake_toppings[2];
+    println!("reference: {reference}");
+
+    let pancake_slice = &pancake_diameters[1..3];
+    println!("{pancake_slice:?}");
+
+    let cake_toppings_slice = &cake_toppings[1..];
+    println!("{cake_toppings_slice:?}");
 }
