1217616a398536c49aedcdf50a8858d4928e7334
Daniel Park <intheimageofyeshua@gmail.com>
2025-10-02 22:48:36 +0900

    The Crate Prefix

--- warehouse/src/inventory.rs
+++ warehouse/src/inventory.rs
@@ -1,9 +1,11 @@
 pub const FLOOR_SPACE: i32 = 10000;
 pub const MANAGER: &str = "Ivan Inventory";
 
+pub mod products;
+
 pub fn talk_to_manager() {
-    println!("Hey, {MANAGER}, how's your coffee?");
+    println!("Hey, {}, how's your coffee?", MANAGER);
+    // println!("{:?}", products::ProductCategory::Ladder);
 }
 
-pub mod products;
 
