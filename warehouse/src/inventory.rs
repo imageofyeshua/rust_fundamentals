1655504b06b8d22cff94c62af56e62f9bc3bdc6b
Daniel Park <intheimageofyeshua@gmail.com>
2025-10-02 23:25:28 +0900

    Alias

--- warehouse/src/inventory.rs
+++ warehouse/src/inventory.rs
@@ -3,7 +3,7 @@ pub mod products;
 pub const FLOOR_SPACE: i32 = 10000;
 pub const MANAGER: &str = "Ivan Inventory";
 
-pub fn talk_to_manager() {
+fn talk_to_manager() {
     println!("Hey, {}, how's your coffee?", MANAGER);
     // println!("{:?}", products::ProductCategory::Ladder);
 }
