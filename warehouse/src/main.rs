d18500e87612cc71e0e2e5ec263f656a089edc25
Daniel Park <intheimageofyeshua@gmail.com>
2025-10-02 18:15:56 +0900

    Create Separate Mod Files

--- warehouse/src/main.rs
+++ warehouse/src/main.rs
@@ -1,27 +1,9 @@
 #![allow(dead_code)]
 
-mod inventory {
-    const FLOOR_SPACE: i32 = 10000;
-    pub const MANAGER: &str = "Ivan Inventory";
-
-    #[derive(Debug)]
-    enum ProductCategory {
-        Ladder,
-        Hammer
-    }
-
-    #[derive(Debug)]
-    struct Item {
-        name: String,
-        category: ProductCategory,
-        quantity: u32
-    }
-
-    fn talk_to_manager() {
-        println!("Hey, {MANAGER}, how's your coffee?");
-    }
-}
+mod inventory;
+mod orders;
 
 fn main() {
     println!("The manager of our inventory is {}", inventory::MANAGER);
+    println!("The manager of our orders is {}", orders::MANAGER);
 }
