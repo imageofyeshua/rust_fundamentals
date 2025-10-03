ec7f51d1b2fd4b375ee59cf6c6184cb77a333d4a
Daniel Park <intheimageofyeshua@gmail.com>
2025-10-03 10:06:15 +0900

    Standard Library Use

--- warehouse/src/main.rs
+++ warehouse/src/main.rs
@@ -3,6 +3,11 @@
 mod inventory;
 mod orders;
 
+use std::{
+    fmt,
+    io::{self, stdin, stdout}
+};
+
 use fake::{Fake, Faker};
 
 use inventory::{Item, ProductCategory, FLOOR_SPACE, MANAGER as INVENTORY_MANAGER};
