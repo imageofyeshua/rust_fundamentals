05b67472197da03531772520a79faf47872a7c70
Daniel Park <intheimageofyeshua@gmail.com>
2025-10-03 10:00:50 +0900

    Fake Crate Usage

--- warehouse/src/main.rs
+++ warehouse/src/main.rs
@@ -3,6 +3,8 @@
 mod inventory;
 mod orders;
 
+use fake::{Fake, Faker};
+
 use inventory::{Item, ProductCategory, FLOOR_SPACE, MANAGER as INVENTORY_MANAGER};
 use orders::MANAGER as ORDERS_MANAGER; 
 
@@ -14,9 +16,17 @@ fn main() {
         FLOOR_SPACE
     );
 
+    let fake_item: Item = Faker.fake();
+    println!("{:?}", fake_item);
+
+    let random_category: ProductCategory = Faker.fake();
+    println!("{:?}", random_category);
+
+    /*
     let favorite_category = ProductCategory::Hammer;
     println!("My favorite category of item is {favorite_category:?}");
 
     let tall_ladder = Item::new(String::from("O-matic-Ladder 2030"), favorite_category, 100);
     println!("{:#?}", tall_ladder);
+    */
 }
