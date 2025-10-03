e42f97304116bd88ce5960dcdfc5f538f777f1b0
Daniel Park <intheimageofyeshua@gmail.com>
2025-10-03 15:02:29 +0900

    Multiple Binary Crates

--- warehouse/src/main.rs
+++ warehouse/src/main.rs
@@ -1,28 +1,3 @@
-#![allow(dead_code)]
-
-use fake::{Fake, Faker};
-
-use warehouse::{Item, ProductCategory, FLOOR_SPACE, INVENTORY_MANAGER, ORDERS_MANAGER};
-
 fn main() {
-    println!(
-        "Our managers are {} and {}. We have {} square feet of floor space",
-        INVENTORY_MANAGER,
-        ORDERS_MANAGER,
-        FLOOR_SPACE
-    );
-
-    let fake_item: Item = Faker.fake();
-    println!("{:?}", fake_item);
-
-    let random_category: ProductCategory = Faker.fake();
-    println!("{:?}", random_category);
-
-    /*
-    let favorite_category = ProductCategory::Hammer;
-    println!("My favorite category of item is {favorite_category:?}");
-
-    let tall_ladder = Item::new(String::from("O-matic-Ladder 2030"), favorite_category, 100);
-    println!("{:#?}", tall_ladder);
-    */
+    println!("This is our primary program");
 }
