70114046517d8d49f5c225076500f70dfa1b02b3
Daniel Park <intheimageofyeshua@gmail.com>
2025-10-04 18:50:06 +0900

    Create Hashmap with New Function

--- hash_maps/src/main.rs
+++ hash_maps/src/main.rs
@@ -1,3 +1,19 @@
+use std::collections::HashMap;
+
 fn main() {
-    println!("Hello, world!");
+    let mut menu: HashMap<String, f64> = HashMap::new();
+
+    menu.insert(String::from("Steak"), 29.99);
+    menu.insert(String::from("Tuna"), 39.99);
+    menu.insert(String::from("Sushi"), 19.99);
+    
+    println!("{:#?}", menu);
+
+    // let mut country_capitals: HashMap<&str, &str> = HashMap::new();
+    let mut country_capitals = HashMap::<&str, &str>::new();
+    country_capitals.insert("France", "Paris");
+    country_capitals.insert("Korea", "Seoul");
+    country_capitals.insert("Germany", "Berlin");
+
+    println!("{:#?}", country_capitals);
 }
