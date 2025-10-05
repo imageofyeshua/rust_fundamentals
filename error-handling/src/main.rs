65d263d4ff7a9cf8bd713623b6e245732f4a10c8
Daniel Park <intheimageofyeshua@gmail.com>
2025-10-05 23:34:03 +0900

    User Input Error Handling

--- error-handling/src/main.rs
+++ error-handling/src/main.rs
@@ -1,8 +1,19 @@
 use std::process;
+use std::io::stdin;
 use std::fs::File;
 
 fn main() {
-    let file = match File::open("story.txt") {
+    println!("Please enter the name of the file:");
+    let mut input = String::new();
+
+    let user_requested_file = stdin().read_line(&mut input);
+
+    if let Err(error) = user_requested_file {
+        eprintln!("Something went wrong: {error:?}");
+        process::exit(1)
+    }
+
+    let file = match File::open(input.trim()) {
         Ok(file) => file,
         Err(error) => {
             eprintln!("Something went wrong: {error:?}");
